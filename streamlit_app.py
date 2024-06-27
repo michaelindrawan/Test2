91c1c04633f3c83589eacd0d5d94065f11842355
michaelindrawan <111202012434@mhs.dinus.ac.id>
2024-06-27 10:26:04 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -91,7 +91,7 @@ elif analysis_type == "Clustering":
         sepal_length = st.number_input('Sepal Length', 4.0, 8.0, 5.5)
         sepal_width = st.number_input('Sepal Width', 2.0, 4.5, 3.0)
         petal_length = st.number_input('Petal Length', 1.0, 7.0, 4.0)
-        petal_width = st.number_input('Petal Width', 0.1, 2.5, 1.0)
+        petal_width = st.number_input('Petal Width', 0.1, 2.5)
 
         input_data = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
 
